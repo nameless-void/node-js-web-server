8420d349ecfcf5e6eabf5d65dcf74923e6690f07
Alexander Seryakov <seryakov78116@gmail.com>
2023-08-08 02:59:52 +0300

    created simple server. returned JSON response

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+const http = require('http')
+
+const PORT = 4000
+
+const responseDataJSON = () => {
+    const data = [{
+        nodeJS: '20.5',
+    }]
+
+    return JSON.stringify(data)
+}
+
+const server = http.createServer((req, res) => {
+    console.log('Server accept request');
+    console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/html');
+
+    if(req.url == '/'){
+        res.end(responseDataJSON())
+    }
+    
+    res.end();
+});
 
 
-
+server.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`Server listen port ${PORT}...`);
+});
